c37a02d678168004aac54371e2a6ab7389f07d16
Chaitany Agrawal <agrawalchaitany+github@gmail.com>
2025-03-23 12:18:28 +0000

    final version of mean var std calculator

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,18 @@
 import numpy as np
 
-def calculate(list):
+def calculate(lst1):
+    
+    if len(lst1) != 9:
+        raise ValueError("List must contain nine numbers.")
 
-
-
-
+    flattened_array=np.array(lst1)
+    lst=np.array(lst1).reshape(3,3)
+    calculations={
+        'mean': [lst.mean(axis=0).tolist(), lst.mean(axis=1).tolist(), flattened_array.mean().item()],
+        'variance': [lst.var(axis=0).tolist(), lst.var(axis=1).tolist(), flattened_array.var().item()],
+        'standard deviation': [lst.std(axis=0).tolist(), lst.std(axis=1).tolist(),flattened_array.std().item()],
+        'max': [lst.max(axis=0).tolist(), lst.max(axis=1).tolist(), flattened_array.max().item()],
+        'min': [lst.min(axis=0).tolist(), lst.min(axis=1).tolist(), flattened_array.min().item()],
+        'sum': [lst.sum(axis=0).tolist(), lst.sum(axis=1).tolist(), flattened_array.sum().item()]
+    }
     return calculations
